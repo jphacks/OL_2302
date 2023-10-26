00799583b1840e8cdc09caf1b05c5983dd4e6487
ishity__ <88872183+ishity@users.noreply.github.com>
2023-10-26 10:33:02 +0900

    Delete pills_app directory

--- pills_app/lib/utils/logger.dart
+++
@@ -1,9 +0,0 @@
-import 'package:logger/logger.dart';
-
-Logger logger = Logger(
-  printer: PrettyPrinter(
-    colors: false, // Colorful log messages
-    printTime: true // Should each log print contain a timestamp
-    ,
-  ),
-);
